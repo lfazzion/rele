2b57d5a6dbe961d5e38f8eb7e38688fe9f746188
Lowell Fazzion <lowellfazzion@gmail.com>
2025-07-07 11:17:17 -0300

    Update Miliohmimetro.ino
    Atualização do multiplexador

--- Miliohmimetro.ino
+++ Miliohmimetro.ino
@@ -6,15 +6,27 @@
 #include "ADS1X15.h"
 #include <Wire.h>
 
+#define A 15
+#define B 2
+#define C 4
+
 ADS1115 ADS(0x48);
 
-const float res_ref = 100.10;
+const float res_ref = 99.781;
 
-float res, r0 = 0;
- 
+static char binaryString[3];
+float res;
+float r0[8] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+int value = 0;
+
+
 void setup() {
   Serial.begin(9600);  // Initialize serial communication with 9600 baud rate
   Wire.begin();        // Initialize I2C communication
+
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
 
   ADS.begin();
   ADS.setGain(0);
@@ -29,13 +41,35 @@ float get_res(){
   return (res_ref*volts)/volts_ref;
 }
 
+
 void calibrate(){
-  for(int i = 0; i<10; i++){
-    r0 += get_res();
+  for(int j = 0; j<8; j++){
+    float r0_base = 0.0;
+    for(int i = 0; i<10; i++){
+      r0_base += get_res();
+    }
+    r0[j] = r0_base/10;
   }
-  r0 = r0/10;
-  Serial.print("Calibração finalizada. Valor R0: ");
-  Serial.println(r0);
+  Serial.print("Calibração finalizada.");
+}
+
+void toBinary(int num) {
+  int pos = 0;
+  for (int i = 2; i >= 0; i--) { // Assuming 8-bit integer
+    if ((num >> i) & 1) {
+      binaryString[pos] = '1';
+    } else {
+      binaryString[pos] = '0';
+    }
+    pos++;
+  }
+}
+
+void multiplx_controler(int OT){
+  toBinary(OT);
+  digitalWrite(A, (binaryString[0] == '1') ? HIGH : LOW);
+  digitalWrite(B, (binaryString[1] == '1') ? HIGH : LOW);
+  digitalWrite(C, (binaryString[2] == '1') ? HIGH : LOW);
 }
 
 void loop() {
@@ -46,15 +80,20 @@ void loop() {
         calibrate();
         break;
       case 'V':
-        res = 0;
-        for(int i = 0; i<20; i++){
-          res += get_res() - r0;
+        for(int j=0; j<2; j++){
+          multiplx_controler(j);
+          delay(500);
+          res = 0;
+          for(int i = 0; i<20; i++){
+            res += get_res() - r0[j];
+          }
+          res = res/20;
+  
+          Serial.print("Resistencia ");
+          Serial.print(j);
+          Serial.print(":\t");
+          Serial.println(res, 3);
         }
-        res = res/20;
-
-        Serial.print("Resistencia: ");
-        Serial.print("\t");
-        Serial.println(res, 3);
         break;
       default:
         Serial.println("Opção inválida");
