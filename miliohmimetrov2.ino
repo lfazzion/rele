e29773bfea478937aa3dbc6e60bf7f807ff0b5f8
AndersonTavare5 <107262266+AndersonTavare5@users.noreply.github.com>
2025-07-17 00:10:50 -0300

    Update miliohmimetrov2.ino
    É isso aí

--- miliohmimetrov2.ino
+++ miliohmimetrov2.ino
@@ -1,51 +1,111 @@
-//Documentação da biblioteca ADS1X15: https://github.com/RobTillaart/ADS1X15
+/* Referências:
+  Documentação da biblioteca ADS1X15: https://github.com/RobTillaart/ADS1X15
+  Manipulação String -> Array: https://arduino.stackexchange.com/questions/77125/convert-string-to-array
+*/
 
 #include "ADS1X15.h"
 #include <Wire.h>
+#include <BluetoothSerial.h>
 
-#define LED_CONT 4
-#define RELAY 2
-#define BUTTON 15
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
-#define MEAN 20
-#define res_ref 99.781
-#define PERIOD 100 
+#define LED_CONT  4
+#define LED_ON    25
+#define RGB_RED   14
+#define RGB_GREEN 27
+#define RGB_BLUE  26
+#define RELAY_DC  2
+#define RELAY_AC  12
+#define BUTTON    15
+
+#define MEAN      20
+#define res_ref   99.781
 
 ADS1115 ADS(0x48);
+BluetoothSerial SerialBT;
 
 float res[2][8] = {0.0};
-float res_cal[8] = {0.0};
+float res_cal = 0.0;
 int value = 0;
 int num = 0;
 bool relay_state = 0;
 
 void setup() {
-  Serial.begin(9600);  // Initialize serial communication with 9600 baud rate
-  Wire.begin();        // Initialize I2C communication
+  Serial.begin(9600);             // Inicializa a comunicação Serial com 9600 baud rate
+  SerialBT.begin("Jiga Rele BT"); // Inicializa a cominucação Serial Bluetooth com o nome "Jiga Rele BT"
+  Wire.begin();                   // Inicializa a comunicação I2C
 
   pinMode(LED_CONT, OUTPUT);
-  pinMode(RELAY, OUTPUT);
+  pinMode(LED_ON, OUTPUT);
+  pinMode(RGB_RED, OUTPUT);
+  pinMode(RGB_GREEN, OUTPUT);
+  pinMode(RGB_BLUE, OUTPUT);
+  pinMode(RELAY_AC, OUTPUT);
+  pinMode(RELAY_DC, OUTPUT);
+  
   pinMode(BUTTON, INPUT);
 
-  digitalWrite(RELAY, 0);
+  digitalWrite(RELAY_DC, 0);
+  digitalWrite(RELAY_AC, 0);
   ADS.begin();
   ADS.setGain(1);
+}
+
+String menssure_time(int relay_type){
+  int16_t val = ADS.readADC_Differential_0_3();
+  unsigned long startTime = micros();
+  while(val == ADS.readADC_Differential_0_3()){digitalWrite(relay_type, 1);}
+  unsigned long endTime = micros();
+  unsigned long duration_on = endTime - startTime;
+
+  unsigned long start = milis();
+  while(milis()-start<1000){}
+
+  val = ADS.readADC_Differential_0_3();
+  startTime = micros();
+  while(val == ADS.readADC_Differential_0_3()){digitalWrite(relay_type, 0);}
+  endTime = micros();
+  unsigned long duration_off = endTime - startTime;
+
+  return "On:"+String(duration_on)+";Off:"+String(duration_off);
+}
+
+void reset_output(){
+  digitalWrite(RGB_RED, 0);
+  digitalWrite(RGB_GREEN, 0);
+  digitalWrite(RGB_BLUE, 0);
+  digitalWrite(RELAY_DC, 0);
+  digitalWrite(RELAY_AC, 0);
+}
+
+void state_RGB(char state){
+  switch(state){
+    case "O":
+      digitalWrite(RGB_RED, 0);
+      digitalWrite(RGB_GREEN, 0);
+      digitalWrite(RGB_BLUE, 1);
+      break;
+    case "B":
+      digitalWrite(RGB_RED, 0);
+      digitalWrite(RGB_GREEN, 1);
+      digitalWrite(RGB_BLUE, 0);
+      break;
+    case "R":
+      digitalWrite(RGB_RED, 1);
+      digitalWrite(RGB_GREEN, 0);
+      digitalWrite(RGB_BLUE, 0);
+      break;
+  }
 }
 
 float get_res(){
   if(ADS.isConnected()){
     ADS.setDataRate(0);    //  0 = slow   4 = medium   7 = fast
-    ADS.setMode(1);
-    do{
-      int16_t val_01 = ADS.readADC_Differential_0_1();
-      int16_t val_13 = ADS.readADC_Differential_1_3();
-      unsigned long startMillis = millis();
-      while(millis()-startMillis<PERIOD){}
-    }while( ( val_01!=ADS.readADC_Differential_0_1()  ) &&
-            ( val_13!=ADS.readADC_Differential_1_3()  )   )
-    
-    val_01 = ADS.readADC_Differential_0_1();
-    val_13 = ADS.readADC_Differential_1_3();
+
+    int16_t val_01 = ADS.readADC_Differential_0_1();
+    int16_t val_13 = ADS.readADC_Differential_1_3();
     float volts_ref = ADS.toVoltage(val_01);
     float volts = ADS.toVoltage(val_13);
 
@@ -56,84 +116,58 @@ float get_res(){
   }
 }
 
-void action_relay(){
+void action_relay(int relay_action){
   relay_state = !relay_state;
-  digitalWrite(RELAY, !relay_state);
+  digitalWrite(relay_action, !relay_state);
 }
 
-unsigned long time_action_relay(){
-  ADS.setDataRate(7);    //  0 = slow   4 = medium   7 = fast
-  ADS.setMode(0);
-  int16_t val_13 = ADS.readADC_Differential_1_3();
-  unsigned long startRelay = millis();
-  action_relay();
-  while(true){
-    int16_t val_13_plus = ADS.readADC_Differential_1_3();
-    if(abs(val_13 - val_13_plus) > val_13*0.5){
-      unsigned long endRelay = millis();
-      break;
-    }
+void calibrate(){
+  wait_confirmation();
+  res_cal = 0.0;
+  for(int i = 0; i<MEAN; i++){
+    res_cal += get_res();
   }
-  action_relay();
-  return endRelay - startRelay
+  res_cal = res_cal/MEAN;
 }
 
-void calibrate(int n){
-  for(int j = 0; j<n; j++){
-    wait_confirmation(j);
-    res_cal[j] = 0.0;
-    for(int i = 0; i<MEAN; i++){
-      res_cal[j] += get_res();
-    }
-    res_cal[j] = res_cal[j]/MEAN;
+void wait_confirmation(int j = 0){
+  if(j==0){
+    SerialBT.println("Preparado para calibração. Pressione o botão para iniciar.");
+  }else{
+    SerialBT.println("Preparado para a próxima leitura do terminal" + String(j) +". Entre com '1' para continuar.");
   }
-  Serial.println("Calibração finalizada.");
-}
-
-void wait_confirmation(int j){
-  Serial.println("Preparado para a próxima leitura do terminal" + String(j) +". Entre com '1' para continuar.");
   digitalWrite(LED_CONT, 1);
   while(digitalRead(BUTTON)==0){}
   digitalWrite(LED_CONT, 0);
 }
 
-void read_voltage(int n){
+void read_voltage(int n, int relay_action){
   for(int k=0; k<2; k++){
     for(int j=0; j<n; j++){
-      wait_confirmation(j);
+      wait_confirmation(j+1);
       res[k][j] = 0.0;
       for(int i = 0; i<MEAN; i++){
-        res[k][j] += get_res() - res_cal[j];
+        res[k][j] += get_res() - res_cal;
       }
       res[k][j] = res[k][j]/MEAN;
     }
-    action_relay();
+    action_relay(relay_action);
   }
 }
 
 void loop() {
-  if(Serial.available()>0){
-    char input = Serial.read();
-    switch(input){
+  if(SerialBT.available()>0){
+    String input = SerialBT.String();
+    switch(input[0]){
       case 'C':
-        Serial.println("Entre com a quantidade de terminais");
-        while(Serial.available()==0){}
-        num = Serial.parseInt();
-        calibrate(num);
+        calibrate();
+        SerialBT.println("Calibração realizada");
         break;
       case 'V':
-        Serial.println("Entre com a quantidade de terminais");
-        while(Serial.available()==0){}
-        num = Serial.parseInt();
-        read_voltage(num);
-        for(int k=0; k<2; k++){
-          for(int i=0; i<num; i++){
-            Serial.println("Resistencia " + String(i) + ":\t" + String(res[k][i], 3));
-          }
-        }
+        
         break;
       default:
-        Serial.println("Opção inválida");
+        SerialBT.println("0");
         break;
     }
   }
